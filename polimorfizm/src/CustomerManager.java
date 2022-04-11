92ab8973d679da94d7160aaf1c107f8e8cab669a
Barış <barisakyildiz3@gmail.com>
2022-04-11 15:53:47 +0300

    Added two new projects

---
+++ polimorfizm/src/CustomerManager.java
@@ -0,0 +1,14 @@
+public class CustomerManager {
+
+    private BaseLogger Logger;
+
+    public CustomerManager(BaseLogger logger){
+        this.Logger = logger;
+    }
+
+    public void Add(){
+        System.out.println("Müşteri Eklendi");
+        this.Logger.Log("log mesajı");
+    }
+
+}
