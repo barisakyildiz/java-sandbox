4870d7f5afa244374123384ab840701b18a2b4aa
barisakyildiz <barisakyildiz3@gmail.com>
2022-05-25 22:43:49 +0300

    added basic up and downcasting

---
+++ TypeCasting/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        long a = 1;
+        int g = 500;
+        double ex = g;
+
+        System.out.println(ex);
+    }
+}
